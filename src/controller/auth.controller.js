7ca492c4e6bced6c501b3d90d9a00935ce6395a5
Phuc <phuctv1112004@gmail.com>
2023-11-07 00:27:56 +0700

    Nop bai thi

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -44,4 +44,50 @@ exports.deleteProduct = async (req, res) => {
       res.status(500).send('Internal Server Error');
     }
 };
+
+// exports.editProduct = async (req, res) => {
+//   const productId = req.params.id;
+//   console.log(productId);
+//   try {
+//     const updatedProduct = await productModel.findByIdAndUpdate(
+//       productId,
+//       req.body,
+//       { new: true }
+//     );
+//     if (!updatedProduct) {
+//       res.status(404).json({ error: 'Product not found' });
+//     } else {
+//       res.json(updatedProduct);
+//     }
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// };
+
+
+exports.putProduct = async (req, res) => {
+  const productId = req.params.id;
+  const updatedData = req.body;
+  console.log(productId);
+
+  try {
+    const updatedProduct = await productModel.findByIdAndUpdate(
+      productId,
+      updatedData,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Render trang chỉnh sửa và truyền dữ liệu sản phẩm đã được cập nhật vào trang
+    res.render('auth/edit', { product: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+
+
  
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,6 +5,10 @@ const controller = require("../controller/auth.controller");
 router.get("/create",controller.product);
 router.post("/create",controller.postProduct);
 
+// router.get("/product/edit/:id", controller.editProduct);
+router.put("/product/edit/:id", controller.putProduct);
+
+
 router.get("/product/delete/:id", controller.deleteProduct)
 
 
